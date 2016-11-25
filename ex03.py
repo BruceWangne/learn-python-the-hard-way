5692ea3a024e7daaec29a8ba641ec4d0d729578d
Bruce <teslawq@users.noreply.github.com>
2016-11-25 16:12:47 +0800

    Add files via upload

---
+++ ex03.py
@@ -0,0 +1,24 @@
+print "I will now count my chickens:"
+
+print "Hens", 25.0+30.0/60.0 # folating point number #25.5
+print "Rosters", 100-25*3%4 #75
+
+print "Now I will count the eggs:"
+
+print 3+2+1-5+4%2-1/4+6 #7
+
+print "Is it true that 2+2<5-7?"
+
+print 3+2<5-7 #False #< compare result as true or false
+
+print "What is 3+2?", 3+2 #5
+print "What is 5-7?", 5-7 #-2
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5>-2 #True
+print "Is it greater or equal?", 5>=-2 #True
+print "Is it less or equal?", 5<=-2 #False
+raw_input("press enter to exit...")
