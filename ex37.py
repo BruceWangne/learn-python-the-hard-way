c2f30d68f1bc5b7803a097a192ec95d1c0b00b9c
Bruce <BruceWangne@users.noreply.github.com>
2016-11-29 15:32:13 +0800

    DU

---
+++ ex37.py
@@ -0,0 +1,92 @@
+print "Just review the code! Consider the past, and you shall know the future!"
+"""and
+del
+from
+not
+while
+as
+elif
+global
+or
+with
+assert
+else
+if
+pass
+yield
+break
+except
+import
+print
+class
+exec
+in
+raise
+continue
+finally
+is
+return
+def
+for
+lambda
+try
+"""
+print "%s" % ("\\")
+print "%s" % ("\'")
+print "%s" % ("\"")
+print "%s" % ("\a")
+print "%s" % ("\b")
+print "%s" % ("\f")
+print "%s" % ("\n")
+print "%s" % ("\r")
+print "%s" % ("\t")
+print "%s" % ("\va")
+"""%d
+%i
+%o
+%u
+%x
+%X
+%e
+%E
+%f
+%F
+%g
+%G
+%c
+%r
+%s
+%%
+"""
+'''+
+-
+*
+**
+/
+//
+%
+<
+>
+<=
+>=
+==
+!=
+<>
+()
+[]
+{}
+@
+,
+:
+.
+=
+;
++=
+-=
+*=
+/=
+//=
+%=
+**=
+'''
+raw_input("press enter to exit...")
