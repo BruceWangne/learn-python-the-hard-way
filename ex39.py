ea8110ec379b18db00ce868b4355221eca4115e2
Bruce <BruceWangne@users.noreply.github.com>
2016-12-02 14:45:14 +0800

    DU

--- ex39.py
+++ ex39.py
@@ -1,24 +1,61 @@
-ten_things = "Apples Oranges Crows Telephone Light Suger"
+# create a mapping of state to abbreviation
+states = {
+	'Oregon': 'OR',
+	'Florida': 'FL',
+	'California': 'CA',
+	'New York': 'NY',
+	'Michigan': 'MI'
+}
 
-print "Wait there's not 10 things in that list, let's fix that."
+# create a basic set of states and some cities in them
+cities = {
+	'CA': 'San Francisco',
+	'MI': 'Detroit',
+	'FL': 'Jacksonville'
+}
 
-stuff = ten_things.split(' ')
-more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
-print "%s" % stuff
-print "%s" % more_stuff
-while len(stuff) != 10:
-	next_one = more_stuff.pop()
-	print "Adding:", next_one
-	stuff.append(next_one)
-	print "There's %d items now." % len(stuff)
+# add some more cities
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
 
-print "There we go: ", stuff
+# print out some cities
+print '-' * 10
+print "NY State has: ", cities['NY']
+print "Or State has: ", cities['OR']
 
-print "Let's do some things with stuff."
+# print some states
+print '-' * 10
+print "Michigan's abbreviation is: ", states['Michigan']
+print "Florida's abbreviation is: ", states['Florida']
 
-print stuff[1]
-print stuff[-1] # whoa! fancy
-print stuff.pop()
-print ' '.join(stuff) # What? cool!
-print '#'.join(stuff[3:5]) #super stellar!
+# do it by using the state then cities dict
+print '-' * 10
+print "Michigan has: ", cities[states['Michigan']]
+print "Florida has: ", cities[states['Florida']]
+
+# print every state abbreviation
+print '-' * 10
+for state, abbrev in states.items():
+	print "%s is abbreviation %s" % (state, abbrev)
+
+# print every city in state
+print '-' * 10
+for abbrev, city in cities.items():
+	print "%s has the city %s" % (abbrev, city)
+
+# now do both at the same time
+print '-' * 10
+for state, abbrev in states.items():
+	print "%s state is abbreviated %s and has city %s" % (state, abbrev, cities[abbrev])
+
+print '-' * 10
+# safely get a abbreviation by state that might not be there
+state = states.get('Texas', None)
+
+if not state:
+	print "Sorry, no Texas."
+
+# get a city with a default value
+city = cities.get('TX', "Does Not Exit")
+print "The city for the state 'TX' is: %s" % city
 raw_input("press enter to exit...")
